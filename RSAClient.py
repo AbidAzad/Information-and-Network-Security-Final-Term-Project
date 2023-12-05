68cc4b1917423c41db7980d4bc9bdeda5903eb74
AbidAzad <officialabidazad@gmail.com>
2023-12-05 01:53:16 -0500

    Added RSA Key Exchange -Abid

---
+++ RSAClient.py
@@ -0,0 +1,262 @@
+import socket
+import threading
+import random
+from tkinter import Tk, Scrollbar, Listbox, Entry, Button, StringVar, DISABLED, NORMAL, Toplevel, Label
+from sympy import isprime
+from gmpy2 import powmod
+
+# Client configuration
+HOST = '192.168.1.31'
+PORT = 55555
+
+# Agreed Upon Values
+primeNumber = 102188617217178804476387977160129334431745945009730065519337094992129677228373
+primitiveRoot = 2
+
+# Create a socket for the client
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+class LFSR:
+    def __init__(self, seed, taps):
+        self.state = seed
+        self.taps = taps
+
+    def shift(self):
+        feedback = sum(self.state[tap] for tap in self.taps) % 2
+        self.state = [feedback] + self.state[:-1]
+        return feedback
+
+    def generate_key(self, length):
+        key = []
+        for _ in range(length):
+            key.append(self.shift())
+        binary_string = ''.join(map(str, key))
+        generated_key = int(binary_string, 2)
+        return generated_key
+
+seed = [1, 0, 1, 0]  
+shiftFeedbackPositions = [0, 2, 3]       
+lfsr = LFSR(seed, shiftFeedbackPositions)
+
+#RSA Functions#
+
+'''Calculates the modular exponetiation of a given message, power, and basis using the 'powmod' function from the gmpy2 library'''
+def exponentiation(message, power, basis):
+    return powmod(message, power, basis)
+
+'''Helper function that incorporates the extended euclidean algorithm to help determine the inverse value within the inverse_finder function.'''
+def extended_gcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, x, y = extended_gcd(b % a, a)
+        return g, y - (b // a) * x, x
+
+'''Finds the modular inverse of a given number a modulo n using the extended euclidean algorithm.'''
+def inverse_finder(a, n):
+    g, x, _ = extended_gcd(a, n)
+    if g != 1:
+        raise ValueError(f"The modular inverse does not exist for {a} modulo {n}")
+    else:
+        return x % n
+'''A function that generates RSA public and private keys. It takes an optional parameter e for the rsaKeyInput; if not provided, it defaults to 3.'''
+def RSA_key_generate():
+    e = 65537
+    p = primitiveRoot
+    q = primeNumber
+    
+    n = p * q    
+    euler = (p-1) * (q-1)  
+    d = inverse_finder(e, euler)
+    publicKey = [e, n]
+    privateKey = [d, n]
+    return publicKey, privateKey
+
+'''A function encrypts a numeric message or a string using RSA encryption with a given key.'''
+def RSA_encrypt(message, key):
+    if not isinstance(message, str):
+        return exponentiation(message, key[0], key[1])
+    elif isinstance(message, str):
+        ciphertext = []
+        for element in range(0, len(message)): 
+            ciphertext.append(int(exponentiation(ord(message[element]), key[0], key[1])))
+        return ciphertext
+
+'''A function decrypts a numeric message or a list of numeric values using RSA decryption with a given key.'''
+def RSA_decrypt(message, key):
+    if not isinstance(message, str) and not isinstance(message, list):
+        return RSA_encrypt(message, key)
+    elif isinstance(message, list):
+        decrpyted = ''
+        for element in range(0, len(message)): 
+            decrpyted+= chr(exponentiation(message[element], key[0], key[1]))
+        return decrpyted
+
+class UsernameGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Enter Username")
+
+        # Label and entry widget for entering the username
+        self.username_label = Label(root, text="Enter your username:")
+        self.username_label.pack(pady=10)
+        self.username_entry = Entry(root, width=30)
+        self.username_entry.pack(pady=10)
+        
+        # Button to submit the username
+        submit_button = Button(root, text="Submit", command=self.submit_username)
+        submit_button.pack(pady=10)
+
+
+    def submit_username(self):
+        username = self.username_entry.get()
+        if username:
+            self.root.destroy()  # Close the username entry GUI
+            # Start the chat GUI with the entered username
+            chat_root = Tk()
+            chat_gui = ChatGUI(chat_root, username)
+            chat_root.mainloop()
+        else:
+            # Display an error message if the username is empty
+            error_label = Label(self.root, text="Please enter a valid username.")
+            error_label.pack(pady=5)
+
+
+class ChatGUI:
+    def __init__(self, root, username):
+        self.root = root
+        self.root.title("Chat Application")
+
+        # Create a listbox to display messages
+        self.message_listbox = Listbox(root, height=20, width=50, selectbackground="white", exportselection=False)
+        self.message_listbox.pack(padx=10, pady=10)
+
+        # Create a scrollbar for the listbox
+        scrollbar = Scrollbar(root)
+        scrollbar.pack(side="right", fill="y")
+
+        # Attach the listbox to the scrollbar
+        self.message_listbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.message_listbox.yview)
+
+        # Create an entry widget for typing messages
+        self.message_entry = Entry(root, width=50)
+        self.message_entry.pack(padx=10, pady=10)
+
+        # Create a Send button to send messages
+        send_button = Button(root, text="Send", command=self.send_message)
+        send_button.pack(pady=10)
+        
+
+        self.public_key, self.private_key = RSA_key_generate()
+        self.recievedLSFRKeys = {}
+        self.sentLSFRKeys = []
+
+        random_seed_length = random.randint(4, 15)  
+        seed = [random.randint(0, 1) for _ in range(random_seed_length)]
+        shiftFeedbackPositions = random.sample(range(len(seed)), k=random.randint(1, len(seed)))
+        shiftFeedbackPositions.sort()
+        lfsr = LFSR(seed, shiftFeedbackPositions)
+        key_length = 16
+        generated_key = lfsr.generate_key(key_length)
+        self.LSFRKey = generated_key
+        message = f"Your generated LFSR Key: {self.LSFRKey}"
+        self.message_listbox.config(state=NORMAL)  # Enable listbox for modification
+        self.message_listbox.insert('end', message)
+        self.message_listbox.config(state=DISABLED)  # Disable listbox after modification
+                
+        # Start a thread to receive messages
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.start()
+        
+        # Send the username to the server
+        client.send(username.encode('utf-8'))
+        client.send(f"Public key: {self.public_key[1]}".encode('utf-8'))
+
+    def send_message(self):
+        message = self.message_entry.get()
+        self.message_entry.delete(0, 'end')
+
+        # Check if the message is a special command to send a private message
+        if message.startswith('./checkSharedKey'):
+            self.check_shared_key(message)
+        elif message.startswith('./sendToUser'):
+            self.send_private_message(message)
+        elif message.startswith('./sendLFSRKey'):
+            self.send_LSFR_key(message)
+        else:
+            client.send(message.encode('utf-8'))
+
+    def receive_messages(self):
+        """Receive and display messages from the server."""
+        while True:
+            try:
+                message = client.recv(1024).decode('utf-8')
+                if message.startswith('./sendLFSRkey'):
+                    parts = message.split(' ', 5)
+                    encryptedLSFRKey = int(parts[2])
+                    decryptionKey = [int(parts[3]), int(parts[4])]
+                    fromUser = parts[5]
+                    sentLSFR = RSA_decrypt(encryptedLSFRKey, decryptionKey)
+                    message = f'Recieved from {fromUser}: {sentLSFR}'
+                    self.recievedLSFRKeys[fromUser] = sentLSFR
+                if message.startswith('./success'):
+                    parts = message.split(' ', 1)
+                    target_username = parts[1]
+                    message = f"LSFR key successfully sent to {target_username}."
+                    self.message_listbox.config(state=NORMAL)  # Enable listbox for modification
+                    self.message_listbox.insert('end', message)
+                    self.message_listbox.config(state=DISABLED)
+                    # Mark the LSFR key as sent to the target user
+                    self.sentLSFRKeys.append(target_username)
+                elif message.startswith('./fail'):
+                    parts = message.split(' ', 1)
+                    target_username = parts[1]
+                    message = f"Failed to send LSFR key to {target_username}."
+                    self.message_listbox.config(state=NORMAL)  # Enable listbox for modification
+                    self.message_listbox.insert('end', message)
+                    self.message_listbox.config(state=DISABLED)                    
+                else:
+                    self.message_listbox.config(state=NORMAL)  # Enable listbox for modification
+                    self.message_listbox.insert('end', message)
+                    self.message_listbox.config(state=DISABLED)  # Disable listbox after modification
+            except Exception as e:
+                print(f"An error occurred while receiving messages: {e}")
+                client.close()
+                break
+
+                
+    def send_private_message(self, message):
+        # Extract username and private message from the message
+        _, target_username, private_message = message.split(' ', 2)
+
+        # Check if the target username is in the list of users
+        if target_username not in self.sentLSFRKeys:
+            print(f"User {target_username} not found in the list.")
+            # Handle the error case as needed (e.g., display an error message)
+            return
+
+        # Send the private message to the target user
+        client.send(message.encode('utf-8'))
+
+    def send_LSFR_key(self, message):
+        _, target_username = message.split(' ', 2)
+
+        # Check if the LSFR key has already been sent to the target user
+        if target_username in self.sentLSFRKeys:
+            print(f"LSFR key already sent to {target_username}.")
+            return
+
+        # Send the LSFR key to the target user
+        encrypted_lsfr_key = RSA_encrypt(self.LSFRKey, self.public_key)
+        client.send(f"./sendLFSRkey {target_username} {encrypted_lsfr_key} {self.private_key[0]} {self.private_key[1]}".encode('utf-8'))
+        
+
+
+
+if __name__ == "__main__":
+    # Start with the username entry GUI
+    username_root = Tk()
+    username_gui = UsernameGUI(username_root)
+    username_root.mainloop()
---
+++ RSAServer.py
@@ -0,0 +1,153 @@
+import socket
+import threading
+import random
+import time
+
+
+# Server configuration
+HOST = '192.168.1.31'
+PORT = 55555
+
+# Lists to store connected clients, their usernames
+clients = []
+usernames = []
+
+# Create a socket for the server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+public_keys_dict = {}
+
+def broadcast(message, sender, publicKey=False):
+    """Send a message to all clients except the sender."""
+    for client in clients:
+        if client != sender:
+            try:
+                if(not publicKey):
+                    client.send(f"[{usernames[clients.index(sender)]}] ".encode('utf-8') + message)
+                else:
+                    client.send(message)
+            except:
+                # Remove the client if unable to send a message
+                handle_disconnect(client)
+
+
+def handle_disconnect(client):
+    """Handle client disconnection."""
+    index = clients.index(client)
+    client.close()
+    username = usernames[index]
+    broadcast(f"{username} has left the chat.".encode('utf-8'), client)  # Call broadcast directly here
+    usernames.remove(username)
+    clients.remove(client)
+    
+
+def handle_client(client, username):
+    """Handle individual client connections."""
+    try:
+        # Broadcast the new user joining the chat
+        broadcast(f"{username} has joined the chat.".encode('utf-8'), client)
+        current_users = ', '.join(usernames)
+        client.send(f"Current users: {current_users}".encode('utf-8'))
+        
+        # Broadcast the public key of the new user to all clients
+        public_key_message = client.recv(1024).decode('utf-8')
+        if public_key_message.startswith("Public key: "):
+            public_key = int(public_key_message[12:])
+            public_keys_dict[username] = public_key
+            broadcast(f"Public key of {username}: {public_key}".encode('utf-8'), client, True)
+
+        else:
+            print("Invalid public key format.")
+            handle_disconnect(client)
+        
+        while True:
+            message = client.recv(1024).decode('utf-8')
+
+            # Announce all current users to the new client
+
+            # Check if the message is a special command to send a private message
+            if message.startswith('./sendToUser'):
+                # Extract the target username and the private message from the command
+                parts = message.split(' ', 2)
+                if len(parts) == 3:
+                    target_username = parts[1]
+                    private_message = parts[2]
+
+                    # Find the target client based on the username
+                    target_client = next((c for c, u in zip(clients, usernames) if u == target_username), None)
+
+                    # Send the private message to the target user
+                    if target_client:
+                        # Remove the command and username, leaving only the private message
+                        cleaned_message = private_message
+                        target_client.send(f"[Private from {username}] ".encode('utf-8') + cleaned_message.encode('utf-8'))
+                    else:
+                        print(f"User not found.")
+                else:
+                    print("Invalid private message format.")
+            
+            if message.startswith('./sendLFSRkey'):
+                # Extract the target username and the private message from the command
+                parts = message.split(' ', 4)
+                if len(parts) == 5:
+                    target_username = parts[1]
+
+                    # Find the target client based on the username
+                    target_client = next((c for c, u in zip(clients, usernames) if u == target_username), None)
+
+                    # Send the private message to the target user
+                    if target_client:
+                        target_client.send(f"{message} {username} ".encode('utf-8'))
+                        response = f'./success {target_username}'
+                    else:
+                        print(f"User not found.")
+                        response = f'./fail {target_username}'
+
+                    # Send the response to the client initiating the command
+                    sender_index = clients.index(client)
+                    sender_username = usernames[sender_index]
+                    sender_client = clients[sender_index]
+                    time.sleep(0.1)
+                    sender_client.send(f'{response} '.encode('utf-8'))
+
+                else:
+                    print("Invalid private message format.")
+                    time.sleep(0.1)
+                    client.send(f'./fail {target_username}'.encode('utf-8'))
+
+
+            else:
+                # Broadcast the message to all clients
+                broadcast(message.encode('utf-8'), client)
+
+    except (socket.error, ConnectionResetError):
+        # Handle client disconnection
+        handle_disconnect(client)
+
+
+
+def start_server():
+    """Start the chat server."""
+    server.listen()
+    print(f"Server is listening on {HOST}:{PORT}")
+
+    while True:
+        # Accept a new client connection
+        client, address = server.accept()
+        print(f"New connection from {address}")
+
+        # Receive the username from the client
+        username = client.recv(1024).decode('utf-8')
+
+        # Add the new client and username to the lists
+        clients.append(client)
+        usernames.append(username)
+
+        # Start a new thread to handle the client
+        thread = threading.Thread(target=handle_client, args=(client, username))
+        thread.start()
+
+
+if __name__ == "__main__":
+    start_server()
